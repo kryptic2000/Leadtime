c86f5537431f87649a309ceba2d68488dcfbaf7d
kryptic2000 <adam@emtorp.se>
2021-03-26 09:25:12 +0100

    Update registration.php
    change vendor name

--- registration.php
+++ registration.php
@@ -1,3 +1,3 @@
 <?php \Magento\Framework\Component\ComponentRegistrar::register(
-\Magento\Framework\Component\ComponentRegistrar::MODULE, 'Blinky_Leadtime',__DIR__
+\Magento\Framework\Component\ComponentRegistrar::MODULE, 'Kryptic2000_Leadtime',__DIR__
 );
